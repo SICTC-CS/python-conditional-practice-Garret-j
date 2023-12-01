2b08c3527c1ccade203f9c1a6c7c557b89771609
Garret-j <146103971+Garret-j@users.noreply.github.com>
2023-12-01 13:16:23 -0600

    Add files via upload

---
+++ GradeCalculator.py
@@ -0,0 +1,16 @@
+percent=int(input("What was the percent you earned on your test/homework? "))
+if percent >= 90:
+  print(f"Percentage on work: {percent}")
+  print("You got an A")
+elif percent >= 80:
+  print(f"Percentage on work: {percent}")
+  print("You got a B")
+elif percent >= 70:
+  print(f"Percentage on work: {percent}")
+  print("You got a C")
+elif percent >= 60:
+  print(f"Percentage on work: {percent}")
+  print("You got a D")
+else:
+  print(f"Percentage on work: {percent}")
+  print("You got an F")
